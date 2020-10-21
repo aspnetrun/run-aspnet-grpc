66c8e52d07a15f76e1f497be7429351b3f8db669
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-21 10:31:09 +0300

    helloworld proto

--- section1/GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
+++ section1/GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
@@ -16,6 +16,7 @@ namespace GrpcHelloWorldServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +32,7 @@ namespace GrpcHelloWorldServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
-                {
+                {                    
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
