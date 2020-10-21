49ee99b6486fae61e698edf73c4cb877b0254953
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-21 09:30:12 +0300

    hello world

--- section1/GrpcHelloWorld/GrpcHelloWorldServer/Program.cs
+++ section1/GrpcHelloWorld/GrpcHelloWorldServer/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GrpcHelloWorld
+namespace GrpcHelloWorldServer
 {
     public class Program
     {
--- section1/GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
+++ section1/GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace GrpcHelloWorld
+namespace GrpcHelloWorldServer
 {
     public class Startup
     {
