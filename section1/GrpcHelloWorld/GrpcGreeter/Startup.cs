3650f2edf10bdfc8feb40faa5f7ae81c3a60bbc3
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-21 15:45:20 +0300

    reflections

--- section1/GrpcHelloWorld/GrpcGreeter/Startup.cs
+++ section1/GrpcHelloWorld/GrpcGreeter/Startup.cs
@@ -17,6 +17,7 @@ namespace GrpcGreeter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddGrpcReflection();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,10 +34,16 @@ namespace GrpcGreeter
             {
                 endpoints.MapGrpcService<GreeterService>();
 
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapGrpcReflectionService();
+                }
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
                 });
+
             });
         }
     }
