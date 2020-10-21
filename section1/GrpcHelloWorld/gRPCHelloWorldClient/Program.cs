2225e8b65945f17d918e6b7299df389a0d04a6b5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-21 12:41:05 +0300

    client

--- section1/GrpcHelloWorld/gRPCHelloWorldClient/Program.cs
+++ section1/GrpcHelloWorld/gRPCHelloWorldClient/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace gRPCHelloWorldClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HelloService.HelloServiceClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "Hello SwN Client" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
