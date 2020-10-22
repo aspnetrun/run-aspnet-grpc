97437623f281be100c4d5f84f0e7e17a889ab865
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-22 09:37:06 +0300

    initial

---
+++ section2/GrpcMicroservices/ProductGrpc/Data/ProductsContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductGrpc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductGrpc.Data
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "William",
+                    Description = "Shakespeare"
+                }
+            );
+        }
+    }
+}
---
+++ section2/GrpcMicroservices/ProductGrpc/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductGrpc.Models
+{
+    public class Product
+    {        
+        public int Id { get; set; }       
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
