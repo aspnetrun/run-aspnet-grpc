5260ac9c91adc8bb5c008d26b7ac8b04679b272b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-22 12:39:56 +0300

    Product model class

--- section2/GrpcMicroservices/ProductGrpc/Models/Product.cs
+++ section2/GrpcMicroservices/ProductGrpc/Models/Product.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProductGrpc.Models
 {
     public class Product
-    {        
-        public int Id { get; set; }       
+    {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Category { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public ProductStatus Status { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+
+    public enum ProductStatus
+    {
+        INSTOCK = 0,
+        LOW = 1,
+        NONE = 2
     }
 }
+
