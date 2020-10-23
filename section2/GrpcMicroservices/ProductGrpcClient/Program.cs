3b3ee9b6ff723767182a6d8e3babca34a3a5066e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-23 12:12:45 +0300

    refactor methods

--- section2/GrpcMicroservices/ProductGrpcClient/Program.cs
+++ section2/GrpcMicroservices/ProductGrpcClient/Program.cs
@@ -3,22 +3,18 @@ using Grpc.Core;
 using Grpc.Net.Client;
 using ProductGrpc.Protos;
 using System;
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
+using static ProductGrpc.Protos.ProductProtoService;
 
 namespace ProductGrpcClient
 {
     class Program
     {
-        static async Task Main(string[] args)
-        {
-            // wait for server is running
-            Console.WriteLine("Waiting for server is running");
-            Thread.Sleep(2000);
 
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new ProductProtoService.ProductProtoServiceClient(channel);
-            
+        private static async Task GetProductAsync(ProductProtoServiceClient client)
+        {
             // GetProductAsync
             Console.WriteLine("GetProductAsync started...");
             var response = await client.GetProductAsync(
@@ -26,31 +22,35 @@ namespace ProductGrpcClient
                                 {
                                     ProductId = 1
                                 });
-            
+
             Console.WriteLine("GetProductAsync Response: " + response.ToString());
             Thread.Sleep(1000);
-
-            // GetAllProducts
-            Console.WriteLine("GetAllProducts started...");
-            using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
-            {
-                while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
-                {
-                    var currentProduct = clientData.ResponseStream.Current;
-                    Console.WriteLine(currentProduct);
-                }
-            }
-            Thread.Sleep(1000);
+        }
+        private static async Task GetAllProducts(ProductProtoServiceClient client)
+        {
+            //// GetAllProducts
+            //Console.WriteLine("GetAllProducts started...");
+            //using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
+            //{
+            //    while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+            //    {
+            //        var currentProduct = clientData.ResponseStream.Current;
+            //        Console.WriteLine(currentProduct);
+            //    }
+            //}
+            //Thread.Sleep(1000);
 
             // GetAllProducts with C# 8
-            Console.WriteLine("GetAllProducts with C#8 started...");                        
+            Console.WriteLine("GetAllProducts with C#8 started...");
             using var clientData2 = client.GetAllProducts(new GetAllProductsRequest());
             await foreach (var responseData in clientData2.ResponseStream.ReadAllAsync())
             {
                 Console.WriteLine(responseData);
             }
             Thread.Sleep(1000);
-
+        }
+        private static async Task AddProductAsync(ProductProtoServiceClient client)
+        {
             // AddProductAsync
             Console.WriteLine("AddProductAsync started...");
             var addProductResponse = await client.AddProductAsync(
@@ -68,7 +68,9 @@ namespace ProductGrpcClient
 
             Console.WriteLine("AddProduct Response: " + addProductResponse.ToString());
             Thread.Sleep(1000);
-
+        }
+        private static async Task UpdateProductAsync(ProductProtoServiceClient client)
+        {
             // UpdateProductAsync
             Console.WriteLine("UpdateProductAsync started...");
             var updateProductResponse = await client.UpdateProductAsync(
@@ -87,49 +89,58 @@ namespace ProductGrpcClient
 
             Console.WriteLine("UpdateProductAsync Response: " + updateProductResponse.ToString());
             Thread.Sleep(1000);
-
+        }
+        private static async Task DeleteProductAsync(ProductProtoServiceClient client)
+        {
             // DeleteProductAsync
             Console.WriteLine("DeleteProductAsync started...");
             var deleteProductResponse = await client.DeleteProductAsync(
                                  new DeleteProductRequest
                                  {
-                                     ProductId = 3                                     
+                                     ProductId = 3
                                  });
 
             Console.WriteLine("DeleteProductAsync Response: " + deleteProductResponse.Success.ToString());
             Thread.Sleep(1000);
-
-            // GetAllProducts with C# 8
-            Console.WriteLine("GetAllProducts with C#8 started...");            
-            using var clientData3 = client.GetAllProducts(new GetAllProductsRequest());
-            await foreach (var responseData in clientData3.ResponseStream.ReadAllAsync())
-            {
-                Console.WriteLine(responseData);
-            }
-            Thread.Sleep(1000);
-
+        }
+        private static async Task InsertBulkProduct(ProductProtoServiceClient client)
+        {
             // InsertBulkProduct
             Console.WriteLine("InsertBulkProduct started...");
             using var clientBulk = client.InsertBulkProduct();
 
-            for(var i = 0; i < 3; i++)
-{
+            for (var i = 0; i < 3; i++)
+            {
                 await clientBulk.RequestStream.WriteAsync(new ProductModel { Name = $"Product{i}" });
             }
             await clientBulk.RequestStream.CompleteAsync();
 
             var responseBulk = await clientBulk;
-            Console.WriteLine($"Status: {responseBulk.Success}. Items: {responseBulk.ProductItems}");
-            // InsertBulkProduct end
+            Console.WriteLine($"Status: {responseBulk.Success}. Items: {responseBulk.ProductItems}");            
             Thread.Sleep(1000);
+        }
 
-            // GetAllProducts with C# 8
-            Console.WriteLine("GetAllProducts with C#8 started...");
-            using var clientData4 = client.GetAllProducts(new GetAllProductsRequest());
-            await foreach (var responseData in clientData4.ResponseStream.ReadAllAsync())
-            {
-                Console.WriteLine(responseData);
-            }
+        static async Task Main(string[] args)
+        {
+            // wait for grpc server is running
+            Console.WriteLine("Waiting for server is running");
+            Thread.Sleep(2000);
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+            await GetProductAsync(client);
+            await GetAllProducts(client);
+            
+            await AddProductAsync(client);
+            await UpdateProductAsync(client);
+            await DeleteProductAsync(client);
+
+            await GetAllProducts(client);
+            
+            await InsertBulkProduct(client);
+
+            //await GetAllProducts(client);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
