bdfcf33f0a8d6c461d5428e07a9a6415361d4ff1
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 16:19:50 +0300

    Discount model

---
+++ section3/GrpcMicroservices/DiscountGrpc/Models/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscountGrpc.Models
+{
+    public class Discount
+    {
+        public int DiscountId { get; set; }
+        public string Code { get; set; }
+        public int Amount { get; set; }
+    }
+}
