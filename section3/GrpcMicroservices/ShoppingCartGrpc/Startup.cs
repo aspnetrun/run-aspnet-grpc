6e541d3fff88752d97b68315a43cb166dbe20253
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 09:58:37 +0300

    inject

--- section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCartGrpc.Services;
 
 namespace ShoppingCartGrpc
 {
@@ -31,7 +32,7 @@ namespace ShoppingCartGrpc
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapGrpcService<GreeterService>();                
+                endpoints.MapGrpcService<ShoppingCartService>();
 
                 endpoints.MapGet("/", async context =>
                 {
