e233787451c5bdddc505ae542586c46d02c8d3a7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 10:00:23 +0300

    service functions added

--- section3/GrpcMicroservices/ShoppingCartGrpc/Services/ShoppingCartService.cs
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Services/ShoppingCartService.cs
@@ -1,4 +1,7 @@
-﻿using ShoppingCartGrpc.Protos;
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,28 @@ namespace ShoppingCartGrpc.Services
 {
     public class ShoppingCartService : ShoppingCartProtoService.ShoppingCartProtoServiceBase
     {
+        private readonly ShoppingCartContext _shoppingCartDbContext;        
+        private readonly ILogger<ShoppingCartService> _logger;
 
+        public ShoppingCartService(ShoppingCartContext shoppingCartDbContext, ILogger<ShoppingCartService> logger)
+        {
+            _shoppingCartDbContext = shoppingCartDbContext ?? throw new ArgumentNullException(nameof(shoppingCartDbContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override Task<ShoppingCartModel> GetShoppingCart(GetShoppingCartRequest request, ServerCallContext context)
+        {
+            return base.GetShoppingCart(request, context);
+        }
+
+        public override Task<AddShoppingCartResponse> AddShoppingCart(IAsyncStreamReader<ShoppingCartModel> requestStream, ServerCallContext context)
+        {
+            return base.AddShoppingCart(requestStream, context);
+        }
+
+        public override Task<RemoveShoppingCartResponse> RemoveShoppingCart(RemoveShoppingCartRequest request, ServerCallContext context)
+        {
+            return base.RemoveShoppingCart(request, context);
+        }
     }
 }
