589013efc34274d194019de3a2bc44e0cfe4e3bb
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 17:32:52 +0300

    GrpcConfigs

---
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Services/DiscountService.cs
@@ -0,0 +1,25 @@
+﻿using DiscountGrpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCartGrpc.Services
+{
+    public class DiscountService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<DiscountModel> GetDiscount(string discountCode)
+        {
+            var discountRequest = new GetDiscountRequest { DiscountCode = discountCode };
+
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
--- section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using DiscountGrpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCartGrpc.Data;
@@ -16,6 +18,13 @@ namespace ShoppingCartGrpc
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,7 +37,10 @@ namespace ShoppingCartGrpc
                 opt.EnableDetailedErrors = true;
             });
 
-            services.AddAutoMapper(typeof(Startup));
+            services.AddAutoMapper(typeof(Startup));            
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcConfigs:DiscountUrl"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
