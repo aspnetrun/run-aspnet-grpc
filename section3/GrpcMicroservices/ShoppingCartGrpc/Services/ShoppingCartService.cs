deb6eeceef55b889ef30e4c4b1da4117904d9773
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 09:57:44 +0300

    ShoppingCartProtoService

--- section3/GrpcMicroservices/ShoppingCartGrpc/Services/ShoppingCartService.cs
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Services/ShoppingCartService.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using ShoppingCartGrpc.Protos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShoppingCartGrpc.Services
 {
-    public class ShoppingCartService
+    public class ShoppingCartService : ShoppingCartProtoService.ShoppingCartProtoServiceBase
     {
+
     }
 }
--- section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Startup.cs
@@ -31,7 +31,7 @@ namespace ShoppingCartGrpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();                
 
                 endpoints.MapGet("/", async context =>
                 {
