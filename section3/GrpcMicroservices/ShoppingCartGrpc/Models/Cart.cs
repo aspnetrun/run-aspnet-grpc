f477cce904be44c17613495facb1e170fac11d0f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-26 09:13:52 +0300

    models added

---
+++ section3/GrpcMicroservices/ShoppingCartGrpc/Models/Cart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCartGrpc.Models
+{
+    public class Cart
+    {
+        public string UserName { get; set; }
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public Cart()
+        {
+        }
+
+        public Cart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+
+    }
+}
