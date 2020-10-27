a7341423775e686063be826bc8b83a243b768784
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-27 10:38:28 +0300

    configs added

--- section3/GrpcMicroservices/ShoppingCartWorkerService/Worker.cs
+++ section3/GrpcMicroservices/ShoppingCartWorkerService/Worker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,18 +12,20 @@ namespace ShoppingCartWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _config;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);                
+                await Task.Delay(_config.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
         }
     }
