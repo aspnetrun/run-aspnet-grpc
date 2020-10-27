d2e286b4f53ed728da5e8d0703d023561fc75aca
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-27 13:33:46 +0300

    Config added

---
+++ section3/GrpcMicroservices/IdentityServer/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+           new ApiScope[]
+           {
+           };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+          };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+          new IdentityResource[]
+          {
+          };
+
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+            };
+    }
+}
