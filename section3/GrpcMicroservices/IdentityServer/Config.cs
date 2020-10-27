549aa0b19a6139a7f76f8b3176755b65c2cd0afd
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-10-27 13:36:53 +0300

    configs added

--- section3/GrpcMicroservices/IdentityServer/Config.cs
+++ section3/GrpcMicroservices/IdentityServer/Config.cs
@@ -12,11 +12,22 @@ namespace IdentityServer
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
+                new Client
+                   {
+                        ClientId = "ShoppingCartClient",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+                        AllowedScopes = { "ShoppingCartAPI" }
+                   }
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
            new ApiScope[]
            {
+               new ApiScope("ShoppingCartAPI", "Shopping Cart API")
            };
 
         public static IEnumerable<ApiResource> ApiResources =>
--- section3/GrpcMicroservices/IdentityServer/Startup.cs
+++ section3/GrpcMicroservices/IdentityServer/Startup.cs
@@ -16,7 +16,10 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer();
+            services.AddIdentityServer()
+             .AddInMemoryClients(Config.Clients)
+             .AddInMemoryApiScopes(Config.ApiScopes)
+             .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
