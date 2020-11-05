f24627bdff9f435bad697f145e3bec638c1f6793
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-11-05 10:18:28 +0300

    discount log operations added

--- section3/GrpcMicroservices/DiscountGrpc/Services/DiscountService.cs
+++ section3/GrpcMicroservices/DiscountGrpc/Services/DiscountService.cs
@@ -1,6 +1,8 @@
 ﻿using DiscountGrpc.Data;
 using DiscountGrpc.Protos;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +10,18 @@ namespace DiscountGrpc.Services
 {
     public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
-        public DiscountService()
-        {
+        private readonly ILogger<DiscountService> _logger;
 
+        public DiscountService(ILogger<DiscountService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public override Task<DiscountModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
             var discount = DiscountContext.Discounts.FirstOrDefault(d => d.Code == request.DiscountCode);
+
+            _logger.LogInformation("Discount is operated with the {discountCode} code and the amount is : {discountAmount}", discount.Code, discount.Amount);
 
             return Task.FromResult(new DiscountModel
             {
